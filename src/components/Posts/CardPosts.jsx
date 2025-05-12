c59365ef3c92cb3ead81ab35aef58fe30e16fdb8
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-11 21:44:03 -0600

    se editan divs

--- src/components/Posts/CardPosts.jsx
+++ src/components/Posts/CardPosts.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
-export const CardPosts = ({title, category, content, author}) => {
+export const CardPosts = ({title, course, datePublication}) => {
+
+  const formattedDate = new Date(datePublication).toLocaleDateString('es-ES', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
   return (
-    <div className="card" style={{width: '18rem'}}>
-        <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <h6 className="card-subtitle mb-2 text-body-secondary">{author}</h6>
-            <p className="card-text">{content}</p>
-            <p className="card-text">{category}</p>
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
+    <div class="card" style={{width: '18rem'}}>
+        <div class="cabody">
+            <h5 class="cardTitle">{title}</h5>
+            <p class="cardCourse">{course}</p>
+            <p class="cardDescription">{course}</p>
+            <p class="cardCourse">{formattedDate}</p>
         </div>
     </div>
   )
