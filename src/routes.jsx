e8dd61118b79774e82bcdaa7e964852d9e978e20
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-11 21:46:15 -0600

    se actualizan rutas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,4 @@
-import { Posts } from "./components/Posts/Posts";
-import { AuthPage } from "./pages/Auth/AuthPage";
-import { FeedPage } from "./pages/Feed/FeedPage";
+
 import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
 import { PostPage } from "./pages/Post/PostPage";
 
