df00946b44225dffa8457792f4a01af41ae34a7c
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 20:40:38 -0600

    se agregan rutas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,17 @@
 
+import { Comments } from "./components/Comments/Comments";
 import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
 import { PostPage } from "./pages/Post/PostPage";
+
 
 export const routes = [
     {
         path: '/',
         element: <PostPage />
+    },
+    {
+        path: '/comments/:postId', 
+        element: <Comments />
     },
     {
         path: '*',
