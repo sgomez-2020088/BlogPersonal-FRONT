a2f05449930f8c0dc54bd44f59d8b1a89bc617d6
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 20:39:50 -0600

    se agregan funcionalidades

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,6 +1,6 @@
-import { CardPosts } from './CardPosts'
+/*import { CardPosts } from './CardPosts'
 import { useContextPosts } from '../../shared/hooks/useContextPosts'
-import { Modal } from './Modal'
+
 import { SyncLoader } from 'react-spinners'
 
 
@@ -25,17 +25,8 @@ export const Posts = () => {
         <h2>Mis publicaciones</h2>
       </div>
       <div class="d-flex align-items-center justify-content-center">
-      { /*
-        <button
-          className="mb-3 btn btn-primary"
-          data-bs-toggle="modal"
-          data-bs-target="#createPostModal"
-        >
-          Agregar post
-        </button>
-        */}
       </div>
-      <Modal />
+      
 
       {}
       {Array.isArray(posts) && posts.length > 0 ? (
@@ -48,6 +39,45 @@ export const Posts = () => {
             link={post.link}
             datePublication={post.datePublication}
           />
+        ))
+      ) : (
+        <p>No hay publicaciones disponibles</p>
+      )}
+    </div>
+  )
+}*/
+
+import React, { useState } from 'react'
+import { useContextPosts } from '../../shared/hooks/useContextPosts' // Usamos el hook de contexto
+import { CardPosts } from './CardPosts'
+import { SyncLoader } from 'react-spinners'
+
+
+export const Posts = () => {
+  const { posts, isFetchingPosts } = useContextPosts() // Usamos el hook para obtener posts y isFetching
+
+  if (isFetchingPosts) {
+    return (
+      <div className="loading">
+        <SyncLoader loading margin={0} size={20} />
+      </div>
+    )
+  }
+
+  return (
+    <div className="row g-0 justify-content-center m-5">
+      <div className="mb-3 d-flex align-items-center justify-content-center">
+        <h2>Mis publicaciones</h2>
+      </div>
+
+      {Array.isArray(posts) && posts.length > 0 ? (
+        posts.map((post) => (
+
+          <CardPosts
+            key={post._id}
+            post={post} // Pasamos toda la información de cada post a CardPosts
+          />
+          
         ))
       ) : (
         <p>No hay publicaciones disponibles</p>
