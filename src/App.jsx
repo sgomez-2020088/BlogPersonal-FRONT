9144151c40ce8ddbf9993675f81b8ead6f31483d
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 20:30:42 -0600

    se modifican elementos de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { useRoutes } from 'react-router-dom'
 import { routes } from './routes'
+import { PostProvider } from './contexts/PostsContext'
+
 
 function App() {
   const elementRoutes = useRoutes(routes)
 
   return (
-    <>
-      {elementRoutes}
-      <Toaster  position='bottom-center' reverseOrder={false}/>
-    </>
+    <PostProvider>
+    {elementRoutes}
+    <Toaster position="bottom-center" reverseOrder={false} />
+  </PostProvider>
   )
 }
 
