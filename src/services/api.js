35dc53ff260a294289e7a5f0f0e379c20b3a805b
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 20:41:53 -0600

    se agren get para comments

--- src/services/api.js
+++ src/services/api.js
@@ -18,3 +18,26 @@ export const getPostRequest = async()=>{
         }
     }
 }
+
+export const getCommentsByPostId = async (postId) => {
+    try {
+        return await apiClient.get(`/post/${postId}/comments`) 
+    } catch (err) {
+        return {
+            error: true,
+            err
+        }
+    }
+}
+
+// Agregar un nuevo comentario
+export const addCommentRequest = async (postId, commentData) => {
+    try {
+        return await apiClient.post(`/post/${postId}/comments`, commentData) 
+    } catch (err) {
+        return {
+            error: true,
+            err
+        }
+    }
+}
