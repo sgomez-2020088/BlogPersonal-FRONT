c281ba6b0473976acb1794781ee8fbd745b0f268
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 20:42:52 -0600

    se crea pagina para comentarios

---
+++ src/pages/Comments/CommentsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Comments } from '../../components/Comments/Comments'
+
+export const PostPage = () => {    
+    return (
+        <div>
+            <h2>Publicaciones</h2>
+            <div>
+                <p>Contenido de la publicación...</p>
+            </div>
+            <Comments />
+        </div>
+    )
+}
