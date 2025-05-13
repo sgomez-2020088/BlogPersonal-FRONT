83ee2df7d0a2c604b20a57670a16838574f30a66
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 20:40:20 -0600

    se agrea mejora para pagina not found

--- src/pages/NotFound/NotFoundPage.jsx
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import './NotFoundPage.css'  // Asegúrate de importar el archivo CSS
 
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className="not-found-container">
+      <h1>404</h1>
+      <p>¡Ups! Página no encontrada</p>
+      <button className="not-found-button" onClick={() => window.location.href = '/'}>
+        Regresar 
+      </button>
+    </div>
   )
 }
