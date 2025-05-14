b48ae2922e24f75d8f1a9632082d23a93ca3f6d7
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-13 19:51:55 -0600

    se borra archivo sin uso

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,67 +1,22 @@
-/*import { CardPosts } from './CardPosts'
+
+import React, { useState, useEffect } from 'react'
 import { useContextPosts } from '../../shared/hooks/useContextPosts'
-
-import { SyncLoader } from 'react-spinners'
-
-
-export const Posts = () => {
-  const { posts } = useContextPosts()
-
-  if (!posts) {
-    return (
-      <div class="loading">
-        <SyncLoader
-          loading
-          margin={0}
-          size={20}
-        />
-      </div>
-    )
-  }
-
-  return (
-    <div class="row g-0 justify-content-center m-5">
-      <div class="mb-3 d-flex align-items-center justify-content-center">
-        <h2>Mis publicaciones</h2>
-      </div>
-      <div class="d-flex align-items-center justify-content-center">
-      </div>
-      
-
-      {}
-      {Array.isArray(posts) && posts.length > 0 ? (
-        posts.map((post) => (
-          <CardPosts
-            key={post._id}
-            title={post.title}
-            course={post.course.name}
-            description={post.description}
-            link={post.link}
-            datePublication={post.datePublication}
-          />
-        ))
-      ) : (
-        <p>No hay publicaciones disponibles</p>
-      )}
-    </div>
-  )
-}*/
-
-import React, { useState } from 'react'
-import { useContextPosts } from '../../shared/hooks/useContextPosts' // Usamos el hook de contexto
 import { CardPosts } from './CardPosts'
 import { SyncLoader } from 'react-spinners'
 
+import './Navbar.css'
 
-export const Posts = () => {
-  const { posts, isFetchingPosts } = useContextPosts() // Usamos el hook para obtener posts y isFetching
+export const Posts = ({ filter }) => {
+  const { posts, isFetchingPosts } = useContextPosts()
+
+  const filteredPosts = filter === 'Todos' ? posts : posts.filter((post) => post.course === filter)
 
   if (isFetchingPosts) {
     return (
       <div className="loading">
         <SyncLoader loading margin={0} size={20} />
       </div>
-    )
+    );
   }
 
   return (
@@ -70,18 +25,13 @@ export const Posts = () => {
         <h2>Mis publicaciones</h2>
       </div>
 
-      {Array.isArray(posts) && posts.length > 0 ? (
-        posts.map((post) => (
-
-          <CardPosts
-            key={post._id}
-            post={post} // Pasamos toda la información de cada post a CardPosts
-          />
-          
+      {Array.isArray(filteredPosts) && filteredPosts.length > 0 ? (
+        filteredPosts.map((post) => (
+          <CardPosts key={post._id} post={post} /> 
         ))
       ) : (
         <p>No hay publicaciones disponibles</p>
       )}
     </div>
-  )
-}
+  );
+};
